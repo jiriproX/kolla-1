1c20234b3a0abcd05d33c155041e6fa26c88ee74
Sean Mooney <sean.k.mooney@intel.com>
2016-12-08 23:57:49 +0000

    introduces OVS with DPDK containers
    - DPDK is a  userspace library for high-performance networking.
    - This change enable support of Open vSwitch with the DPDK
      accelerated netdev datapath.
    - This change provided binary support only by default
      and allows source build via a template-override.
    - This change provides an example template override file
      and corresponding documentation.
    
    Co-Authored-By: Mauricio Lima <mauriciolimab@gmail.com>
    Partial-Implements: blueprint ovs-dpdk
    Change-Id: I7c3a5dddeca1886fe1c7681ce8d5bebde08f3102

---
+++ docker/ovsdpdk/ovsdpdk-db/ovs_ensure_configured.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "This placeholder script should be replaced with one approprite for the deployment tool in use."
+echo "If you are seeing this msg an error has occured."
+
+exit 1
