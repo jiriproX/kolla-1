4ab988fb65fc75df8f900895fdd33bd20a5cba34
Martin Matyas <martinx.maty@intel.com>
2017-01-12 05:25:28 -0800

    DPDK adaptations to make ansible consistent with kolla images
    Signed-off-by: Martin Matyas <martinx.maty@intel.com>

--- docker/ovsdpdk/ovsdpdk-db/ovs_ensure_configured.sh
+++ docker/ovsdpdk/ovsdpdk-db/ovs_ensure_configured.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-echo "This placeholder script should be replaced with one approprite for the deployment tool in use."
-echo "If you are seeing this msg an error has occured."
+bridge=$1
+port=$2
+ovs_datapath=${3:-"system"}
+dpdk_lcore_mask=${4:-"0x02"}
+pmd_cpu_mask=${5:-"0x06"}
+dpdk_socket_mem=${6:-"2048"}
+interface_type=""
 
-exit 1
+ovs-vsctl br-exists $bridge; rc=$?
+if [[ $rc == 2 ]]; then
+    changed=changed
+    ovs-vsctl --no-wait add-br $bridge -- set bridge $bridge datapath_type=$ovs_datapath
+fi
+
+if [[ ! $(ovs-vsctl list-ports $bridge) =~ $(echo "\<$port\>") ]]; then
+    changed=changed
+    cmd="ovs-vsctl --no-wait add-port $bridge $port"
+    #NOTE(sean-k-mooney): the dpdk port name must be dpdk0
+    [[ "$ovs_datapath" == "netdev" ]] && cmd="ovs-vsctl --no-wait add-port $bridge dpdk0 -- set interface dpdk0 type=dpdk"
+    $cmd
+fi
+
+if [[ "$ovs_datapath" == "netdev" ]]; then
+    ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=$pmd_cpu_mask other_config:dpdk-init=True other_config:dpdk-lcore-mask=$dpdk_lcore_mask \
+    other_config:dpdk-mem-channels=4 other_config:dpdk-socket-mem=$dpdk_socket_mem other_config:dpdk-hugepage-dir=/dev/hugepages
+fi
+
+echo $changed
